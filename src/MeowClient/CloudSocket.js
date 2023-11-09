51ad6d58e2bcc16335993e50d32339b0befda794
userfriend <userfriendanonymous@protonmail.com>
2023-11-09 13:57:11 +0600

    cloudSocket

---
+++ src/MeowClient/CloudSocket.js
@@ -0,0 +1,26 @@
+import { CloudConnection } from "meowclient";
+import { objectKeysToCamelCaseV2 as objToCamelCase } from 'keys-converter'
+
+let catch_ = (ok, err, f) => () => {
+    try {
+        return ok(f())
+    } catch(e) {
+        if (!(e instanceof Error)) {
+            throw new Error("Error must be an instance of Error")
+        }
+        return err(e)
+    }
+}
+
+export let initImpl = ok => err => id => session =>
+    catch_(ok, err, () => new CloudConnection(session, id))
+
+export let session = socket => socket.session
+
+export let id = socket => socket.id
+
+export let setVarImpl = ok => err => name => value => socket =>
+    catch_(ok, err, () => socket.setVariable(name, value))
+
+export let varImpl = ok => err => name => socket =>
+    catch_(ok, err, () => socket.getVariable(name))
---
+++ src/MeowClient/CloudSocket.purs
@@ -0,0 +1,35 @@
+module MeowClient.CloudSocket where
+
+import Prelude
+
+import Data.Argonaut (Json, decodeJson)
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
+import Effect.Exception (Error)
+import MeowClient.Session as Session
+import MeowClient.Utils (LeftF, RightF)
+
+foreign import data Value :: Type
+
+foreign import initImpl :: RightF -> LeftF -> Int -> Session.Value -> Effect (Either Error Value)
+
+init :: Int -> Session.Value -> Effect (Either Error Value)
+init = initImpl Right Left
+
+foreign import session :: Value -> Session.Value
+foreign import id :: Value -> Int
+
+foreign import setVarImpl :: RightF -> LeftF -> String -> String -> Value -> Effect (Either Error Unit)
+
+setVar :: String -> String -> Value -> Effect (Either Error Unit)
+setVar = setVarImpl Right Left
+
+foreign import varImpl :: RightF -> LeftF -> String -> Value -> Effect (Either Error Json)
+
+var :: String -> Value -> Effect (Either Error (Maybe String))
+var n v = map
+    ( decodeJson >>> case _ of
+        Left _ -> Nothing
+        Right o -> o
+    ) <$> varImpl Right Left n v
