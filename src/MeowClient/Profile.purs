cd6947181118e101cafda1031e9f1c90708a8b81
userfriend <userfriendanonymous@protonmail.com>
2023-11-16 15:24:00 +0600

    Project & Profile doc comments

--- src/MeowClient/Profile.purs
+++ src/MeowClient/Profile.purs
@@ -28,6 +28,9 @@ import MeowClient.Utils (LeftF, RightF, decodeJsErrorOrJson)
 import Promise (Promise)
 import Promise.Aff (toAffE)
 
+-- | Profile pointer.
+-- |
+-- | ### Example
 type Value =
     { username :: String
     , session :: Session.Value
@@ -35,51 +38,144 @@ type Value =
 
 foreign import statusImpl :: RightF -> LeftF -> Value -> Effect (Promise (Either Error Json))
 
+-- | Gets profile status (scratcher / new scratcher / scratch team).
+-- | 
+-- | `status [profile]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- status profile
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right status -> -- ...
+-- | ```
 status :: Value -> Aff (Either JsonOrJsError Status.Value)
 status v = decodeJsErrorOrJson <$> (toAffE $ statusImpl Right Left v)
 
 foreign import followImpl :: RightF -> LeftF -> Value -> Effect (Promise (Either Error Unit))
 
+-- | Follow a user.
+-- | 
+-- | `follow [profile]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- follow profile
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right _ -> -- ...
+-- | ```
 follow :: Value -> Aff (Either Error Unit)
 follow v = toAffE $ followImpl Right Left v
 
 foreign import unfollowImpl :: RightF -> LeftF -> Value -> Effect (Promise (Either Error Unit))
 
+-- | Unfollows a user.
+-- | 
+-- | `unfollow [profile]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- unfollow profile
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right _ -> -- ...
+-- | ```
 unfollow :: Value -> Aff (Either Error Unit)
 unfollow v = toAffE $ unfollowImpl Right Left v
 
 foreign import commentImpl :: RightF -> LeftF -> Int -> Int -> String -> Value -> Effect (Promise (Either Error Unit))
 
+-- | Leaves a comment on a profile.
+-- | 
+-- | `comment [commentee id] [parent id] [content] [profile]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- comment 0 0 "Hello!" profile
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right _ -> -- ...
+-- | ```
 comment :: Int -> Int -> String -> Value -> Aff (Either Error Unit)
-comment c pid cid v = toAffE $ commentImpl Right Left c pid cid v
+comment commenteeId parentId content v = toAffE $ commentImpl Right Left commenteeId parentId content v
 
 foreign import deleteCommentImpl :: RightF -> LeftF -> Int -> Value -> Effect (Promise (Either Error Unit))
 
+-- | Deletes a comment on a profile.
+-- | 
+-- | `deleteComment [comment id] [profile]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- deleteComment 104027540 profile
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right _ -> -- ...
+-- | ```
 deleteComment :: Int -> Value -> Aff (Either Error Unit)
 deleteComment id v = toAffE $ deleteCommentImpl Right Left id v
 
 foreign import apiImpl :: RightF -> LeftF -> Value -> Effect (Promise (Either Error Json))
 
+-- | Gets api information of a profile.
+-- | 
+-- | `api [profile]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- api profile
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right info -> -- ...
+-- | ```
 api :: Value -> Aff (Either JsonOrJsError Api.Value)
 api v = decodeJsErrorOrJson <$> toAffE (apiImpl Right Left v)
 
 foreign import messagesCountImpl :: RightF -> LeftF -> Value -> Effect (Promise (Either Error Json))
 
+-- | Gets messages' count of a user.
+-- | 
+-- | `messagesCount [profile]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- messagesCount profile
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right count -> -- ...
+-- | ```
 messagesCount :: Value -> Aff (Either JsonOrJsError Int)
 messagesCount v = decodeJsErrorOrJson <$> toAffE (messagesCountImpl Right Left v)
 
 foreign import commentsImpl :: RightF -> LeftF -> Int -> Value -> Effect (Promise (Either Error Json))
 
+-- | Gets comments on a profile.
+-- | 
+-- | `comments [page] [profile]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- comments 1
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right comments' -> -- ...
+-- | ```
 comments :: Int -> Value -> Aff (Either JsonOrJsError (Array Comment.Value))
-comments p v = decodeJsErrorOrJson <$> toAffE (commentsImpl Right Left p v)
+comments page v = decodeJsErrorOrJson <$> toAffE (commentsImpl Right Left page v)
 
 foreign import toggleCommentingImpl :: RightF -> LeftF -> Value -> Effect (Promise (Either Error Unit))
 
--- | ## Toggles profile commenting
--- | *Requires authentication with the same username*
+-- | Toggles profile commenting.
+-- | 
+-- | `toggleCommenting [profile]`
 -- | ### Example
--- | ```
--- | toggleCommenting { username : "yourUsername", session }
+-- | ```purescript
+-- | do
+-- |    result <- toggleCommenting profile
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right _ -> -- ...
 -- | ```
 toggleCommenting :: Value -> Aff (Either Error Unit)
 toggleCommenting v = toAffE $ toggleCommentingImpl Right Left v
--- src/MeowClient/Project.purs
+++ src/MeowClient/Project.purs
@@ -95,7 +95,7 @@ commentReplies offset limit id v = decodeJsErrorOrJson <$> toAffE (commentReplie
 
 foreign import commentImpl :: RightF -> LeftF -> Int -> Int -> String -> Value -> EffPromise (Either Error Unit)
 
--- | Leaves a comment.
+-- | Leaves a comment on a project.
 -- | 
 -- | `comment [commentee id] [parent id] [content] [project]`
 -- | ### Example
@@ -107,59 +107,180 @@ foreign import commentImpl :: RightF -> LeftF -> Int -> Int -> String -> Value -
 -- |        Right _ -> -- ...
 -- | ```
 comment :: Int -> Int -> String -> Value -> Aff (Either Error Unit)
-comment c pi ci v = toAffE $ commentImpl Right Left c pi ci v
+comment commenteeId parentId content v = toAffE $ commentImpl Right Left commenteeId parentId content v
 
 foreign import setCommentingImpl :: RightF -> LeftF -> Boolean -> Value -> EffPromise (Either Error Unit)
 
+-- | Sets commenting of a project (open or closed).
+-- | 
+-- | `setCommenting [is open?] [project]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- setCommenting false project
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right _ -> -- ...
+-- | ```
 setCommenting :: Boolean -> Value -> Aff (Either Error Unit)
-setCommenting a v = toAffE $ setCommentingImpl Right Left a v
+setCommenting isOpen v = toAffE $ setCommentingImpl Right Left isOpen v
 
 foreign import setTitleImpl :: RightF -> LeftF -> String -> Value -> EffPromise (Either Error Unit)
 
+-- | Sets title of a project.
+-- | 
+-- | `setTitle [content] [project]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- setTitle "New title!" project
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right _ -> -- ...
+-- | ```
 setTitle :: String -> Value -> Aff (Either Error Unit)
-setTitle a v = toAffE $ setTitleImpl Right Left a v
+setTitle content v = toAffE $ setTitleImpl Right Left content v
 
 foreign import setInstructionsImpl :: RightF -> LeftF -> String -> Value -> EffPromise (Either Error Unit)
 
+-- | Sets instructions of a project.
+-- | 
+-- | `setInstructions [content] [project]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- setInstructions "Use WASD to move!" project
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right _ -> -- ...
+-- | ```
 setInstructions :: String -> Value -> Aff (Either Error Unit)
-setInstructions a v = toAffE $ setInstructionsImpl Right Left a v
+setInstructions content v = toAffE $ setInstructionsImpl Right Left content v
 
 foreign import setNotesAndCreditsImpl :: RightF -> LeftF -> String -> Value -> EffPromise (Either Error Unit)
 
+-- | Sets "Notes and Credits" of a project.
+-- | 
+-- | `setNotesAndCredits [content] [project]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- setNotesAndCredits "Thanks to everyone!" project
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right _ -> -- ...
+-- | ```
 setNotesAndCredits :: String -> Value -> Aff (Either Error Unit)
-setNotesAndCredits a v = toAffE $ setNotesAndCreditsImpl Right Left a v
+setNotesAndCredits content v = toAffE $ setNotesAndCreditsImpl Right Left content v
 
 foreign import setThumbnailImpl :: RightF -> LeftF -> Buffer -> Value -> EffPromise (Either Error Unit)
 
+-- | Sets thumbnail of a project.
+-- |
+-- | `setThumbnail [buffer] [project]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- setThumbnail buffer project
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right _ -> -- ...
+-- | ```
 setThumbnail :: Buffer -> Value -> Aff (Either Error Unit)
-setThumbnail a v = toAffE $ setThumbnailImpl Right Left a v
+setThumbnail buffer v = toAffE $ setThumbnailImpl Right Left buffer v
 
 foreign import shareImpl :: RightF -> LeftF -> Value -> EffPromise (Either Error Unit)
 
+-- | Shares a project.
+-- |
+-- | `share [project]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- share project
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right _ -> -- ...
+-- | ```
 share :: Value -> Aff (Either Error Unit)
 share v = toAffE $ shareImpl Right Left v
 
 foreign import unshareImpl :: RightF -> LeftF -> Value -> EffPromise (Either Error Unit)
 
+-- | Unshares a project.
+-- |
+-- | `unshare [project]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- unshare project
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right _ -> -- ...
+-- | ```
 unshare :: Value -> Aff (Either Error Unit)
 unshare v = toAffE $ unshareImpl Right Left v
 
 foreign import isLovingImpl :: RightF -> LeftF -> Value -> EffPromise (Either Error Json)
 
+-- | Checks whether logged in user is loving a project.
+-- |
+-- | `isLoving [project]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- isLoving project
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right isLoving' -> -- ...
+-- | ```
 isLoving :: Value -> Aff (Either JsonOrJsError Boolean)
 isLoving v = decodeJsErrorOrJson <$> toAffE (isLovingImpl Right Left v)
 
 foreign import isFavoritingImpl :: RightF -> LeftF -> Value -> EffPromise (Either Error Json)
 
+-- | Checks whether logged in user is favoriting a project.
+-- |
+-- | `isFavoriting [project]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- isFavoriting project
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right isFavoriting' -> -- ...
+-- | ```
 isFavoriting :: Value -> Aff (Either JsonOrJsError Boolean)
 isFavoriting v = decodeJsErrorOrJson <$> toAffE (isFavoritingImpl Right Left v)
 
 foreign import setLovingImpl :: RightF -> LeftF -> Boolean -> Value -> EffPromise (Either Error Unit)
 
+-- | Loves/unloves a project.
+-- |
+-- | `setLoving [is loving?] [project]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- setLoving true project
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right _ -> -- ...
+-- | ```
 setLoving :: Boolean -> Value -> Aff (Either Error Unit)
-setLoving i v = toAffE $ setLovingImpl Right Left i v
+setLoving state v = toAffE $ setLovingImpl Right Left state v
 
 foreign import setFavoritingImpl :: RightF -> LeftF -> Boolean -> Value -> EffPromise (Either Error Unit)
 
+-- | Favorites/unfavorites a project.
+-- |
+-- | `setFavoriting [is favoriting?] [project]`
+-- | ### Example
+-- | ```purescript
+-- | do
+-- |    result <- setFavoriting true project
+-- |    case result of
+-- |        Left error -> -- ...
+-- |        Right _ -> -- ...
+-- | ```
 setFavoriting :: Boolean -> Value -> Aff (Either Error Unit)
-setFavoriting i v = toAffE $ setFavoritingImpl Right Left i v
+setFavoriting state v = toAffE $ setFavoritingImpl Right Left state v
